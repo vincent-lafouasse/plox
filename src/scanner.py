1cbbdd45aaef2cf9e21342ad00713981b59c1dcb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-24 20:16:42 +0100

    type hint

--- src/scanner.py
+++ src/scanner.py
@@ -1,6 +1,9 @@
+from typing import List
+from token import Token
+
 class Scanner:
     def __init__(self, source: str):
         pass
 
-    def scanTokens(self):
+    def scanTokens(self) -> List[Token]:
         return []
